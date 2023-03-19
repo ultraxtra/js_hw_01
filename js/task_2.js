8d9a63bc52034878b3cf65f57a11ec168095d847
Artem Melnychuk <temamel@ukr.net>
2023-03-19 19:49:42 +0100

    task_2

---
+++ js/task_2.js
@@ -0,0 +1,10 @@
+const total = 100;
+const ordered = 130;
+//const ordered = 20;
+
+if(ordered > total){
+    console.log("На складі недостатня кількість товарів");
+}
+else{
+    console.log("Замовлення оформлено, з вами звяжеться менеджер");
+}
